6227a1722dae9f76648bc4f0951088f47c4855ac
mrOtoM <mrotomachala@gmail.com>
2022-10-20 14:48:01 +0200

    JS - first logic containerMovements

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // DATA
 const account1 = {
   owner: 'Oto Machala',
-  movements: [100, 200, 300, -500, -123, 10],
+  movements: [100, 200, 300, -500, -123, 10, 650, -2300],
   interestRate: 1.2,
   pin: 1111,
 };
@@ -28,3 +28,25 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+
+// Elements
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+//Logic
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ''; // hide actual value from index.html
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}
+        </div>
+        <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
